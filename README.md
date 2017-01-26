5fa0541c537aea512ecff7494522d0bdb1517790
izhbannikov <zhbannikov.ilya@gmail.com>
2017-01-25 20:23:05 -0500

    First commit

---
+++ README.md
@@ -0,0 +1,14 @@
+In this folder we provide examples of usage of R-package "haploR". These examples are in the paper as well.
+
+The folder contains the following files:
+
+* haploR_example.R - an R-script to run the examples.
+* Files example1-4.csv - these files contain an output for each particular example (Example1-4).
+* README.md - a readme file.
+
+
+Study used in Example 3: 
+
+Athanasiadis G et al.
+Genetic determinants of plasma β2-glycoprotein I levels: a genome-wide association study in extended pedigrees from Spain. J Thromb Haemost 2013; 11: 521–8.
+http://onlinelibrary.wiley.com/doi/10.1111/jth.12120/full
---
+++ haploR_examples.R
@@ -0,0 +1,22 @@
+# Examples used in the paper "haploR: an R-package for querying web-based annotation tools"#
+
+library(haploR)
+
+# Example 1: input vector of SNPs #
+res <- queryHaploreg(query=c("rs10048158","rs4791078"))
+write.csv(res, "~/Dropbox/haploR/Manuscript/f1000research_submission/examples/example1.csv")
+
+# Example 2: Input text file with SNPs #
+res <- queryHaploreg(file=system.file("extdata/snps.txt", package="haploR"))
+write.csv(res, "~/Dropbox/haploR/Manuscript/f1000research_submission/examples/example2.csv")
+
+# Example 3: Using a particular study #
+#Get a list of studies
+studies <- getStudyList()
+#Query Hploreg
+res <- queryHaploreg(study=studies[[2]])
+write.csv(res, "~/Dropbox/haploR/Manuscript/f1000research_submission/examples/example3.csv")
+
+# Example 4: Querying RegulomeDB #
+res <- queryRegulome(query=c("rs4791078","rs10048158"), format="full")
+write.csv(res, "~/Dropbox/haploR/Manuscript/f1000research_submission/examples/example4.csv")
